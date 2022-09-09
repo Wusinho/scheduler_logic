b7677cf5507462bd7dc418810c46f216b79d6a01
Heber Lazo <heberlazo@gmail.com>
2022-09-09 16:32:53 -0500

    Working cumulative hrs user progression

--- spec/lib/array_list_spec.rb
+++ spec/lib/array_list_spec.rb
@@ -24,7 +24,7 @@ describe 'Array List' do
     arr.add(@worker_2.worker_id, @worker_2.working_hours_counter)
     arr.add(@worker_2.worker_id, @worker_2.working_hours_counter)
     arr.add(@worker_2.worker_id, @worker_2.working_hours_counter)
-    # arr.add(@worker_1.worker_id, @worker_2.working_hours_counter)
+    arr.add(@worker_1.worker_id, @worker_2.working_hours_counter)
     # arr.add(@worker_2.worker_id, @worker_2.working_hours_counter)
 
     # arr.add(@worker_3.worker_id, @worker_3.working_hours_counter)
