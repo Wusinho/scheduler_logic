6fc880264980cbcee828cb0d482e9ee7f17e4ad2
Heber Lazo <heberlazo@gmail.com>
2022-09-12 21:39:56 -0500

    Added general configurations

--- lib/deparment_configuration.rb
+++ lib/deparment_configuration.rb
@@ -2,9 +2,16 @@
 
 # Department configuration
 class DepartmentConfiguration
-  attr_accessor :max_hours_per_worker
+  attr_reader :max_hours_per_worker
 
   def initialize
     @max_hours_per_worker = 8
   end
+
+  def change_max_hours(num)
+    return unless num.instance_of?(Integer)
+
+    @max_hours_per_worker = num
+  end
+
 end
